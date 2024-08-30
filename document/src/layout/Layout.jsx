7de66ed50c6874931389156aa375ef779036b8cc
Dami K <damikang@kakao.com>
2024-08-30 13:37:32 +0900

    fix: [KAN-99] #comment ✅Document sub menu improved. (public path adapted. 2차 수정) - 24.08.30

--- document/src/layout/Layout.jsx
+++ document/src/layout/Layout.jsx
@@ -2,13 +2,14 @@ import HeaderMenu from '@/layout/HeaderMenu'
 import HeaderMenuComponents from '@/layout/HeaderMenu.component'
 import { Outlet } from 'react-router-dom'
 
-function Layout() {
+function Layout({ children }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <HeaderMenu />
       <div className="flex flex-col">
       <HeaderMenuComponents />
-      <Outlet />
+      {/* <Outlet /> */}
+      { children }
       </div>
    </div>
   )
